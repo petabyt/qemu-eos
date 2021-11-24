27ccd8dab29626d07c27b627a7f8d519513d15ff
Daniel <brikbusters@gmail.com>
2021-11-24 13:37:05 -0500

    qemu-eos now replaces run_canon_fw.sh bash magic

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
+# Semi replacement for install.sh - 
+# For easier modification
+
 QEMU_DIR=qemu-eos
 QEMU_NAME=qemu-4.2.1
-
 DIR=../qemu-eos
 
-all: clean init patch
+all: clean init patch compile
 
 init:
-	-mkdir $(DIR)
+	-@mkdir $(DIR)
 	cd $(DIR); wget -nc https://download.qemu.org/$(QEMU_NAME).tar.bz2
 	cd $(DIR); tar jxf $(QEMU_NAME).tar.bz2
 
 PATCH_DIR=$(DIR)/$(QEMU_NAME)
 CODEBASE_URL=https://raw.githubusercontent.com/reticulatedpines/magiclantern_simplified/dev
 patch:
-	cd $(PATCH_DIR); mkdir -p hw/eos
+	-@mkdir -p $(PATCH_DIR)/hw/eos
+	-@mkdir -p $(PATCH_DIR)/hw/eos/dbi
 
 	# Copy scripts
 	cp -r scripts/* $(DIR)
@@ -24,10 +27,6 @@ patch:
 	cd $(DIR); unxz -v sd.img.xz
 	cd $(DIR); cp -v sd.img cf.img
 
-	# Compile qemu-eos CLI program - Inserts the
-	# qemu directory into the program as a constant
-	sudo gcc -DQEMU_DIR='"$(shell cd $(DIR); pwd)"' qemu-eos.c -o /bin/qemu-eos
-
 	# Patch remotely from magiclantern_simplified
 	cd $(PATCH_DIR)/hw/eos/dbi; wget -4 $(CODEBASE_URL)/src/backtrace.c
 	cd $(PATCH_DIR)/hw/eos/dbi; wget -4 $(CODEBASE_URL)/src/backtrace.h
@@ -37,6 +36,11 @@ patch:
 	patch -N -p1 --directory=$(PATCH_DIR) < $(QEMU_NAME).patch
 	cp -r eos/* $(PATCH_DIR)/hw/eos
 
+qemu-eos:
+	# Compile qemu-eos CLI program - Inserts the
+	# qemu directory into the program as a constant
+	sudo gcc -DQEMU_DIR='"$(shell cd $(DIR); pwd)"' qemu-eos.c -o /bin/qemu-eos
+
 # Compile QEMU
 CPU=$(shell grep -c processor /proc/cpuinfo 2> /dev/null || sysctl -n hw.ncpu 2> /dev/null || echo 1)
 compile:
@@ -45,4 +49,6 @@ compile:
 	
 clean:
 	rm -rf *.tar.bz2*
+
+	# Remove everything except qemu tar
 	cd $(DIR); ls | grep -v $(QEMU_NAME).tar.bz2 | xargs rm -rf
--- qemu-eos.c
+++ qemu-eos.c
@@ -6,10 +6,37 @@
 	#define QEMU_DIR "~/qemu-eos"
 #endif
 
+// Get DebugMsg_log address
+int findDebug(char address[], char buffer[]) {
+	FILE *f = fopen(address, "r");
+	if (f == NULL) {
+		return -1;
+	}
+
+	/* Go through the file, and find the "0xC14" around this:
+	# b *0xC14
+	# DebugMsg_log
+	*/
+
+	char line[128];
+	while (fgets(line, 128, f) != NULL) {
+		if (!strncmp(line, "# b *", 5)) {
+			char line2[128];
+			fgets(line2, 128, f);
+			if (!strncmp(line2, "# DebugMsg_log", 14)) {
+				// Increment past "# b *", and strip newline
+				strcpy(buffer,
+					strtok(line + 5, "\n")
+				);
+			}
+		}
+	}
+}
+
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		puts("Usage: ./qemu-eos <ACTION>");
-		puts("Actions: run, mount");
+		puts("Actions: emu, mount");
 		return -1;
 	}
 
@@ -20,11 +47,33 @@ int main(int argc, char *argv[]) {
 			return -1;
 		}
 
+		char output[64];
+		findDebug("1300D/debugmsg.gdb", output);
+
 		snprintf(cmd, sizeof(cmd),
-		"cd %s; ./run_canon_fw.sh %s,firmware=\"boot=1\"", QEMU_DIR, argv[2]);
+			// Send the debug 
+			"cd %s; env QEMU_EOS_DEBUGMSG=\"%s\"" \
+			" qemu-*/arm-softmmu/qemu-system-arm" \
+			" -drive if=sd,format=raw,file=sd.img" \
+			" -drive if=ide,format=raw,file=cf.img" \
+			" -chardev socket,server,nowait,path=qemu.monitor$QEMU_JOB_ID,id=monsock" \
+			" -mon chardev=monsock,mode=readline" \
+			" -name %s" \
+			" -M %s,firmware=\"boot=1\"",
+			QEMU_DIR,
+			output,
+			argv[2], argv[2]
+		);
 
 		system(cmd);
 	} else if (!strcmp(argv[1], "mount")) {
+		if (system("which kpartx")) {
+			puts("Can't find kpartx. Try installing util-linux in your package manager.");
+			return -1;
+		}
+
+		puts("Will mount and unmount with sudo.");
+
 		system("sudo kpartx -av sd.img");
 		puts("Do copy files, press enter to unmount.");
 		fgetc(stdin);
